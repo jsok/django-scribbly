bb234989d38a80afb63847f6d32c7a03509cfec1
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2012-08-23 18:54:20 +1000

    Update to bootstrap 2.1

--- scribbly/static/bootstrap-2.0/js/bootstrap-button.js
+++ scribbly/static/bootstrap-2.0/js/bootstrap-button.js
@@ -1,5 +1,5 @@
 /* ============================================================
- * bootstrap-button.js v2.1.0
+ * bootstrap-button.js v2.0.4
  * http://twitter.github.com/bootstrap/javascript.html#buttons
  * ============================================================
  * Copyright 2012 Twitter, Inc.
